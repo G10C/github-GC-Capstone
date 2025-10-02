cdf106bc8d52c2ff262b5e838086197f83dd2797
Giovanni <giocheris63@gmail.com>
2025-10-02 13:50:30 -0400

    Updated Arena/ implemented critical and miss features

--- src/ArenaRPG/Arena.java
+++ src/ArenaRPG/Arena.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Arena {
-    Scanner input = new Scanner(System.in);
+    private final Scanner input = new Scanner(System.in);
     private final Random rng = new Random();
-    private static final int Atk_Crit_Chance = 20; // 20% chance to triple damage
-    private static final int Atk_Miss_Chance = 10; // 10% chance to miss
 
-    private boolean roll(int percent){
+    // Tweak these to balance combat
+    private static final int CRIT_CHANCE_PERCENT = 20; // 20% = triple damage
+    private static final int MISS_CHANCE_PERCENT = 10; // 10% = no damage
+
+    private boolean roll(int percent) {
         return rng.nextInt(100) < percent;
     }
 
-    private int applyHitModifier(int basePower){
-        if (roll(Atk_Miss_Chance)){
-            return 0; // missed
+    // Apply miss/crit and print feedback for the attacker
+    private int applyHitModifiers(int basePower, String attackerLabel) {
+        if (roll(MISS_CHANCE_PERCENT)) {
+            System.out.println(attackerLabel + " missed!");
+            return 0;
         }
         int power = basePower;
-        if (roll(Atk_Crit_Chance)){
-            power *= 3; // critical hit
+        if (roll(CRIT_CHANCE_PERCENT)) {
+            System.out.println("Critical hit by " + attackerLabel + "!");
+            power *= 3;
         }
         return power;
     }
-
 
     public void battle(Warrior fighter, Enemy opponent) {
 
@@ -33,37 +37,49 @@ public class Arena {
                     ", Base Endurance: " + fighter.baseDefense +
                     ", Battle Power: " + fighter.playerPower() +
                     ", Equipped Weapon: " + (fighter.getWeapon() != null ? fighter.getWeapon().weaponName : "None"));
-            System.out.println("Enemy's stats: " + "\nBase Power: " + opponent.baseStrength + ", Base Endurance: " + opponent.baseDefense + ", Battle Power: " + opponent.enemyPower());
+            System.out.println("Enemy's stats: " +
+                    "\nBase Power: " + opponent.baseStrength +
+                    ", Base Endurance: " + opponent.baseDefense +
+                    ", Battle Power: " + opponent.enemyPower());
+
             System.out.println("Player Turn.");
             System.out.println("Your opponent Stands before you, what will you do?\n(on your keyboard type: z to attack, x to defend, or c to forfeit)");
             String action = input.nextLine();
+
             if (action.equals("z")) {
-                opponent.takeHit(fighter.playerPower());
+                // Player attacks
+                int playerBase = fighter.playerPower();
+                int playerMod = applyHitModifiers(playerBase, "you");
+                opponent.takeHit(playerMod);
 
-            }
-            if (action.equals("z") && opponent.isAlive()) {
-                System.out.println("Enemy Turn");
-                fighter.takeHit(opponent);
-            }
-            
-            if (action.equals("x")) {
-                fighter.reducedHit(opponent);
-            }
+                // Enemy retaliates if alive
+                if (opponent.isAlive()) {
+                    System.out.println("Enemy Turn");
+                    int enemyBase = opponent.enemyPower();
+                    int enemyMod = applyHitModifiers(enemyBase, "the enemy");
+                    fighter.takeHit(enemyMod); // updated signature
+                }
 
+            } else if (action.equals("x")) {
+                // Player defends; enemy attacks with reduced effect
+                int enemyBase = opponent.enemyPower();
+                int enemyMod = applyHitModifiers(enemyBase, "the enemy");
+                fighter.reducedHit(enemyMod); // updated signature
 
-            if (action.equals("c")) {
+            } else if (action.equals("c")) {
                 System.out.println("There is always next time, I suppose...\n(if you wish to play again, please rerun the code.)");
                 return;
+
+            } else {
+                System.out.println("Invalid input. Please choose z, x, or c.");
             }
 
             if (fighter.health <= 0) {
-                System.out.println("You have fallen in battle and lost...\n(if you wish to play again, please rerun the code.)");// to make up for no replay method.
+                System.out.println("You have fallen in battle and lost...\n(if you wish to play again, please rerun the code.)");
             }
             if (opponent.health <= 0) {
                 System.out.println("You have bested your opponent and won!!\n(if you wish to play again, please rerun the code.)");
             }
-
-
         }
     }
 }
